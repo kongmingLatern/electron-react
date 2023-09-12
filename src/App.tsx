cc60c4277c05f7c036e8562dd76d56aea2010a6b
凤之兮原 <1577310596@qq.com>
2023-09-12 19:47:00 +0800

    feat: add Speech module

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import LoginForm from './components/form/LoginForm'
 import { Icon } from '@iconify/react'
-import { Space } from 'antd'
+import Speech from './module/Speech'
 
 function App() {
 	console.log(
@@ -24,6 +24,8 @@ function App() {
 				</h3>
 				<LoginForm />
 			</div>
+
+			<Speech />
 		</>
 	)
 }
